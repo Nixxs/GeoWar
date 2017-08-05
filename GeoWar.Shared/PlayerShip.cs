42670d4600140ade403456e19215800094855bb6
Nicholas Chai (Reynolds) <nick@nickchai.com>
2017-08-05 10:20:59 +0800

    Created the player class it is a singleton which takes advantage of the other classes we have created up til now.
    Art for defining it's texture and uses the gameroots screensize to position itself also it inherits from the entity class

---
+++ GeoWar.Shared/PlayerShip.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoWar
+{
+    class PlayerShip : Entity
+    {
+        private static PlayerShip _instance;
+
+        // if the player instance property hasn't already been created then create one
+        // and return that value, otherwise just return the object that had been assigned
+        // earlier
+        public static PlayerShip Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PlayerShip();  
+                }
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// This is a struct i think, which is almost like a class within a class
+        /// i have to assume it inherits everything from the parent class "entity"
+        /// and we define anything else we would want as well then it can be used
+        /// as the creation of the player class. Its like a constructor for the player.
+        /// that allows the user to create a player object without having to use the 
+        /// new keyword.
+        /// </summary>
+        private PlayerShip()
+        {
+            image = Art.Player;
+            Position = GameRoot.ScreenSize / 2;
+            Radius = 10;
+        }
+
+        public override void Update()
+        {
+            //ship logic goes here
+        }
+    }
+}
